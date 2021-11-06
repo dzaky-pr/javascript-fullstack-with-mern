e86b5907edd4ce456a892fa91dcbb16ef76ad11b
Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>
2021-11-06 22:25:36 +0700

    Add Componentm etc
    Signed-off-by: Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Beranda from './components/Beranda';
+import Navbar from './components/Navbar';
+import Manajemenbuku from './components/ManajemenBuku';
+import { useState } from 'react';
 function App() {
+  const [books, setBooks] = useState([
+    { _id: 1, judul: 'Laskar Pelangi', pengarang: 'Andrea Hirata', harga: 80000, stok: 7 },
+    { _id: 2, judul: 'Bumi', pengarang: 'Tere Liye', harga: 85000, stok: 5 },
+  ]);
+
+  function storeData(inputBook) {
+    console.log(inputBook);
+    alert('Data Berhasil Ditambahkan');
+  }
+
+  function updateData(inputBook) {
+    console.log(inputBook);
+    alert('Data Berhasil Diupdate');
+  }
+
+  function deleteData(book) {
+    console.log(book);
+    alert('Data Berhasil Dihapus');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/">
+            <Beranda />
+          </Route>
+
+          <Route exact path="/manajemen-buku">
+            <Manajemenbuku bookList={books} store={storeData} update={updateData} remove={deleteData} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
